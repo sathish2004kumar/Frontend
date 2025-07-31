c238279ccc150411f740ff5d78060be7d156868c
Sathish Kumar <sathish71845@gmail.com>
2025-07-31 08:18:48 +0530

    Training

---
+++ day2.js
@@ -0,0 +1,7 @@
+console.log(0.1+0.2+0.3);
+console.log(0.1+0.4+0.1);
+console.log(0.2+0.4+0.1);
+console.log(0.1+0.5+0.1);
+console.log(0.1+0.2+0.9);
+console.log(0.1+0.2+0.10);
+
---
+++ rod.js
@@ -0,0 +1,100 @@
+// String and number
+console.log("sk"<2);
+console.log("sk">2);
+console.log("sk">=2);
+console.log("sk"<=2);
+console.log("sk"==2);
+console.log("sk"!=2);
+
+//String and boolean
+
+console.log("Ap">false);
+console.log("Ap"==true);
+console.log("Ap"==false);
+console.log("Ap"!=true);
+console.log("Ap"!=false);
+
+//string and null
+console.log("MS">null);
+console.log("MS"<null);
+console.log("MS">=null);
+console.log("MS"<=null);
+console.log("MS"==null);
+console.log("MS"!=null);
+
+//string and undefined
+console.log("AK">undefined);
+console.log("AK"<undefined);
+console.log("AK">=undefined);
+console.log("AK"<=undefined);
+console.log("AK"==undefined);
+console.log("AK"!=undefined);
+
+//number and boolean (true=1 , false=0)
+console.log(0<true);
+console.log(1<true);
+console.log(0<false);
+console.log(1<false);
+console.log(0>true);
+console.log(1>true);
+console.log(0>false);
+console.log(1>false);
+console.log(1>=true);
+console.log(1>=false);
+console.log(1<=true);
+console.log(1<=false);
+console.log(1==true);
+console.log(1==false);
+console.log(1!=true);
+console.log(1!=false);
+
+//number and null (null value of empty is considerd to value of 0)
+console.log(1<null);
+console.log(1>null);
+console.log(1<=null);
+console.log(1>=null);
+console.log(1==null);
+console.log(1!=null);
+
+//number and undefined (undefined is empty considerd of no values)
+console.log(1<undefined);
+console.log(1>undefined);
+console.log(1<=undefined);
+console.log(1>=undefined);
+console.log(1==undefined);
+console.log(1!=undefined);
+
+//boolean and null
+console.log(true<null);
+console.log(false>null);
+console.log(true<=null);
+console.log(true>=null);
+console.log(true==null);
+console.log(true!=null);
+
+//boolean and undefined
+console.log(true<undefined);
+console.log(false>undefined);
+console.log(true<=undefined);
+console.log(true>=undefined);
+console.log(true==undefined);
+console.log(true!=undefined);
+
+//null and undefined
+console.log(null>undefined);
+console.log(null<undefined);
+console.log(null>=undefined);
+console.log(null<=undefined);
+console.log(null==undefined);
+console.log(null!=undefined);
+
+
+
+
+
+
+
+
+
+
+
